226e1e3bba7765d0f8a6cd2cb576eb41129c7f6f
maitrung0104 <maitrung0104@gmail.com>
2025-02-28 23:47:32 +0700

    Initial commmit

---
+++ src/main/java/com/example/fragrance_shop/FragranceShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.fragrance_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragranceShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FragranceShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fragrance_shop/FragranceShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fragrance_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragranceShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
