a9b01f2a3c32913283058a9591f43283e1dbe518
maitrung0104 <maitrung0104@gmail.com>
2025-03-24 13:18:34 +0700

    Fix the pom.xml and connect DB

---
+++ fragrance/src/main/java/com/example/fragrance/FragranceApplication.java
@@ -0,0 +1,13 @@
+package com.example.fragrance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FragranceApplication.class, args);
+	}
+
+}
