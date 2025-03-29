796f900e050f6f0f4e9b76bb2adda87e55210e3f
maitrung0104 <maitrung0104@gmail.com>
2025-03-29 16:33:51 +0700

    feat: Product create, get, getALL

---
+++ fragrance/src/main/java/com/example/fragrance/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.example.fragrance.controller;
+
+import com.example.fragrance.model.Product;
+import com.example.fragrance.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+    @Autowired
+    ProductService productService;
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+}
---
+++ fragrance/src/main/java/com/example/fragrance/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.example.fragrance.service;
+
+import com.example.fragrance.model.Product;
+import com.example.fragrance.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+}
