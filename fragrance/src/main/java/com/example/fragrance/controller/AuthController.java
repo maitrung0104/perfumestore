cc88e3568a5cf820cdcde2ddae53249ed5776271
maitrung0104 <maitrung0104@gmail.com>
2025-04-03 16:12:45 +0700

    HotFix feat: Sign in and Register

--- fragrance/src/main/java/com/example/fragrance/controller/AuthController.java
+++ fragrance/src/main/java/com/example/fragrance/controller/AuthController.java
@@ -7,14 +7,16 @@ import com.example.fragrance.model.User;
 import com.example.fragrance.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
-public class UserController {
+public class AuthController {
     @Autowired
     private UserService userService;
 
