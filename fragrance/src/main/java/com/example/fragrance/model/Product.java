c221c88f8215fc8bc1ffd8a562b5ae913dfe7e71
maitrung0104 <maitrung0104@gmail.com>
2025-04-20 01:55:49 +0700

    Feat:Authentication

--- fragrance/src/main/java/com/example/fragrance/model/Product.java
+++ fragrance/src/main/java/com/example/fragrance/model/Product.java
@@ -16,7 +16,7 @@ public class Product {
 
     private String name;
     private String brand;
-    private Long price;
+    private Double price;
     private String description;
     private String imageUrl;
 
